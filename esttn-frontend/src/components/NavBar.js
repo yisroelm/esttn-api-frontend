9b33b66280a69cceb9708ecd7f368f5ea4dd2b66
yisroel96 <yisroel96@gmail.com>
2019-07-25 10:57:20 -0400

    Changed Home to Les

--- esttn-frontend/src/components/NavBar.js
+++ esttn-frontend/src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
     <Container>
         <br/>
         <div class='navbar' class='center'>
-          <Link to='/' exact><Button size='massive' color='purple'>Home</Button></Link>
+          <Link to='/' exact><Button size='massive' color='purple'>Les</Button></Link>
           <Link to='/clients' exact><Button size='massive' color='red'>Clients</Button></Link>
           <Link to='/products' exact><Button size='massive' color='blue'>Products</Button></Link>
           <Link to='/appointments' exact><Button size='massive' color='grey'>Appointments</Button></Link>
